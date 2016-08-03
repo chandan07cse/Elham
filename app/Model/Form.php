8e60cb1131feefb4c58fd79a125702deb113d5a1
chandan07cse <freak.arian@gmail.com>
2016-08-04 05:21:57 +0600

    demo model added

---
+++ app/Model/Form.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sysadmin
+ * Date: 8/1/16
+ * Time: 12:12 PM
+ */
+namespace Elham\Model;
+use Carbon\Carbon;
+use config\PDO;//For PDO Queries
+use Illuminate\Database\Eloquent\Model as Eloquent;//For Eloquent Queries
+use Illuminate\Database\Capsule\Manager as Capsule;//For Query Builder
+class Form extends Eloquent{
+    //use PDO;
+    protected $fillable=['username','email','password','image'];//remember the format
+    protected $userName,$email,$passWord,$imageName;
+    public $timestamps = false;
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setPassWord($passWord)
+    {
+        $this->passWord = $passWord;
+    }
+
+    public function getPassWord()
+    {
+        return $this->passWord;
+    }
+
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+    public function insert()
+    {
+        $command = Form::create([
+            'username'=>$this->getUserName(),
+            'email'=>$this->getEmail(),
+            'password'=>$this->getPassWord(),
+            'image'=>$this->getImageName()
+        ]);
+//        using Query Builder
+//       $command = Capsule::table('forms')->insert([
+//           'id'=>null,
+//           'username'=>'Noor',
+//           'email'=>'freak.arian@gmail.com',
+//           'password'=>'chandan07cse@!',
+//           'image'=>'images/me.jpg'
+//       ]);
+        return $command ? true : false;
+    }
+}
