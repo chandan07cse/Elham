1648dcbe89a44baf731adff30a3d425cbdd4ea04
chandan07cse <freak.arian@gmail.com>
2016-08-09 18:39:14 +0600

    template model updated

---
+++ db/seeds/UserSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Carbon\Carbon;
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+//        $data = array(
+//            array(
+//                'email'    => 'freak.arian@gmail.com',
+//                'password' => md5('me'),
+//                'created_at' => Carbon::now()
+//            ),
+//            array(
+//                'email'    => 'chandan07cse@gmail.com',
+//                'password' => md5('chandan07cse@!'),
+//                'active' => 1,
+//                'created_at' => Carbon::now(),
+//            )
+//        );
+
+        $faker = Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 100; $i++) {
+            $data[] = [
+                'email'      => $faker->email,
+                'password'      => sha1($faker->password),
+                'created_at'       => date('Y-m-d H:i:s'),
+            ];
+        }
+
+        $this->insert('users', $data);
+    }
+
+//        $users = $this->table('users');
+//        $users->insert($data)
+//              ->save();
+    //}
+}
