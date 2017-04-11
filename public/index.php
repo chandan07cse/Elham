625136d2af1d1a3aa7eb3ff8bb3c090cfcff8305
Md Ainul Moin Noor <freak.arian@gmail.com>
2017-04-11 16:11:29 +0600

    session cookie vulnerability issue fixed

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 ini_set('display_errors',1);
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
 error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT );
 require __DIR__ . '/../bootstrap/front.php';
 
