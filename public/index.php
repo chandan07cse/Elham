6c2c1d3c9398aaea8c91661bbff4be2a0bc8be66
Md Ainul Moin Noor <freak.arian@gmail.com>
2017-04-20 01:11:20 +0600

    session cookie should be disabled for the first time of project init comment added

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 ini_set('display_errors',1);
+/*
+ * active these 2 lines after project init
+ * */
 //ini_set('session.cookie_httponly', 1);
 //ini_set('session.cookie_secure', 1);
 ini_set('memory_limit','16M');
